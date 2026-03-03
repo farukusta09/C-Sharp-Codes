ea5d1411dab0974d8c6186fa2971df6a2d502c96
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2026-03-04 01:25:57 +0300

    Add password control with if-else statement

---
+++ 08_Password_Control_If_Else.cs
@@ -0,0 +1,17 @@
+Console.Write("Şifreyi Giriniz: ") ;
+ string password;
+ password = Console.ReadLine();
+
+ if (password == "abcd")
+{
+    
+Console.WriteLine("Şifre Doğru...");
+
+}
+
+else
+{
+    
+Console.WriteLine("Şifre Yanlış...");
+
+}
