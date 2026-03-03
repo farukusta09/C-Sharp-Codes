e60552112921ddc63400bd824d2a0a063e8ec61d
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2026-03-04 01:28:07 +0300

    Add integer comparison with if-else statement

---
+++ 10_Integer_Comparison_If_Else.cs
@@ -0,0 +1,17 @@
+int number;
+
+Console.Write("Sayıyı Giriniz: ");
+number = int.Parse(Console.ReadLine());
+
+if (number==5)
+{
+    
+Console.WriteLine("Sayı Doğru...");
+
+}
+else
+{
+    
+Console.WriteLine("Sayı Yanlış...");
+
+}
