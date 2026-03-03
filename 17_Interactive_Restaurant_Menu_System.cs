7fd0a30ef2b2160bc3790f51882bad0ffe2b0838
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2026-03-04 01:37:53 +0300

    Implement interactive restaurant menu system

---
+++ 17_Interactive_Restaurant_Menu_System.cs
@@ -0,0 +1,76 @@
+
+
+Console.WriteLine(" ***** C# Eğitim Kampı Restoran ***** ");
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine();
+Console.WriteLine("1 - Ana Yemekler");
+Console.WriteLine("2 - Çorbalar");
+Console.WriteLine("3 - Salatalar");
+Console.WriteLine("4 - Pizzalar");
+Console.WriteLine("5 - İçecekler");
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------");
+
+string menuItem;
+Console.WriteLine();
+Console.Write("Menüden Görmek İstediğiniz Menü İçin Bir Sayı Giriniz: ");
+menuItem = Console.ReadLine();
+
+if ( menuItem == "1" )
+{
+
+    Console.WriteLine();
+    Console.WriteLine("1 - Patlıcan Musakka");
+    Console.WriteLine("2 - Kara Şimşek");
+    Console.WriteLine("3 - Yoğurtlu Kızartma");
+    Console.WriteLine();
+    
+
+}
+
+if ( menuItem == "2" )
+{
+
+    Console.WriteLine();
+    Console.WriteLine("1 - Ezogelin");
+    Console.WriteLine("2 - Mercimek");
+    Console.WriteLine();
+    
+
+}
+
+if ( menuItem == "3" )
+{
+
+    Console.WriteLine();
+    Console.WriteLine("1 - Tavuklu Salata");
+    Console.WriteLine("2 - Ton Balıklı Salata");
+    Console.WriteLine();
+    
+
+}
+
+if ( menuItem == "4" )
+{
+
+    Console.WriteLine();
+    Console.WriteLine("1 - Kavurmalı Pizza");
+    Console.WriteLine("2 - Karışık Pizza");
+    Console.WriteLine("3 - Bol Sucuklu Pizza");
+    Console.WriteLine();
+    
+
+}
+
+if ( menuItem == "5" )
+{
+
+    Console.WriteLine();
+    Console.WriteLine("1 - Kola");
+    Console.WriteLine("2 - Fanta");
+    Console.WriteLine("3 - Limonata");
+    Console.WriteLine();
+    
+
+}
