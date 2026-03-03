29eb02b077d84d7bf40989752cc52ca592e8061e
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2026-03-04 01:27:02 +0300

    Create 09_Country_Capital_Validation.cs

---
+++ 09_Country_Capital_Validation.cs
@@ -0,0 +1,19 @@
+string capital, country;
+
+Console.Write("Başkenti Gİriniz: ");
+capital = Console.ReadLine();
+
+Console.Write("Ülkeyi Giriniz: ");
+country = Console.ReadLine();
+
+if ( capital == "ankara" & country == "türkiye")
+{
+    
+Console.WriteLine("Bilgiler Doğrulandı...");
+
+}
+
+else
+{
+    
+Console.WriteLine("Hatalı Giriş...");
