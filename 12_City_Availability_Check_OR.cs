d1b877794239fe21a5be7a0b05c3db9ba6f4f243
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2026-03-04 01:33:25 +0300

    Add city availability check for Istanbul, Ankara, and Bursa

---
+++ 12_City_Availability_Check_OR.cs
@@ -0,0 +1,14 @@
+
+string city;
+Console.Write("Lütfen Şehir Giriniz: ");
+city = Console.ReadLine();
+
+if ( city == "istanbul"| city == "ankara" | city =="bursa")
+{
+    Console.WriteLine("şehir mevcut...");
+}
+else
+{
+    
+Console.WriteLine("şehir mevcut değil...");
+}
