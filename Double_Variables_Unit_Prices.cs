2826f1e4448eb11ebd0cb239f8e227c2521cbdf9
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2026-03-04 01:07:07 +0300

    Add unit prices for various fruits and vegetables

---
+++ Double_Variables_Unit_Prices.cs
@@ -0,0 +1,13 @@
+double applePrice, orangePrice, strawberryPrice, tomatoPrice, potatoPrice;
+
+ applePrice = 3.87;
+ orangePrice = 2.85;
+ strawberryPrice = 1.63;
+ tomatoPrice = 5.98;
+ potatoPrice = 3.78;
+
+ Console.WriteLine("Elma Birim Fİyatı: " + applePrice );
+ Console.WriteLine("Portakal Birim Fİyatı: " + orangePrice );
+ Console.WriteLine("Çilek Birim Fİyatı: " + strawberryPrice );
+ Console.WriteLine("Domates Birim Fİyatı: " + tomatoPrice );
+ Console.WriteLine("Patates Birim Fİyatı: " + potatoPrice );
